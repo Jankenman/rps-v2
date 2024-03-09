d255140b630b88810cb47c3378a88318f8618a4e
Jankenman <120563461+Jankenman@users.noreply.github.com>
2024-03-09 18:49:01 +0900

    ヘッダーを追加

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "gatsby";
+import * as styleUtils from "../util.module.css";
+import * as styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className={`${styles.header} ${styleUtils.wrapper}`}>
+        <h1>
+          <Link to="/">RPS Apps</Link>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">トップ</Link>
+            </li>
+            <li>
+              <Link to="/terms/terms">利用規約</Link>
+            </li>
+            <li>
+              <Link to="/terms/privacy">プライバシーポリシー</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
